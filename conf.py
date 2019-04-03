bc6f734ebc8f292f4efe1770dde9680eeeb5f4f3
Sebastian Riehm <hallo@herrriehm.de>
2019-04-03 17:33:55 +0200

    changed nikola SITE_URL

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Sebastian Riehm"  # (translatable)
 BLOG_TITLE = "7-zip.de"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://sevenzip.uber.space/"
+SITE_URL = "https://7-zip.de/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://7-zip.de/"
